804310467924c9c875b499e175b9628346f2bfb9
JustSomeCarbon <noe.garcia@student.nmt.edu>
2021-04-16 16:25:19 -0600

    looking for bugs

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main():
     while (usr != "quit"):
         usr = input("> ")
         expr = functionModule.parseExpression(usr)
-        result = functionModule.evaluation(expr, 0)
+        #result = functionModule.evaluation(expr, 0)
         print(">> ", expr)
+        #print("result", result)
     # END OF WHILE
 
     print("> bye")
