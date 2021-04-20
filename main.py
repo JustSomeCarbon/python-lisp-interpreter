0dbe2a312e6e0ca41d5081b2bf1c5112b6a4ac9f
JustSomeCarbon <noe.garcia@student.nmt.edu>
2021-04-19 18:27:27 -0600

    udpated for use

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     while (usr != "(quit)"):
         usr = input("> ")
         expr = functionModule.parseExpression(usr, 0)
-        # print(expr)  # for debugging
+        # print(expr)  # for parseExpression debugging
         if expr == []:
             print(">> NIL")
             continue
         if expr[0] != "(quit)":
             expr = functionModule.swapExpression(expr)
-            print(expr)
+            # print(expr) # for swapExpression debugging
             result = functionModule.evaluation(expr, 0)
             print(">> ", result)
     # END OF WHILE
